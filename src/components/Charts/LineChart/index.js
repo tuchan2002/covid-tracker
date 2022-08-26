6320bd488977e5c40cd6c45a021ad4575b90b2db
TranAnhTu17 <20020495@vnu.edu.vn>
2022-08-26 10:49:37 +0700

    add Deaths line for Charts

--- src/components/Charts/LineChart/index.js
+++ src/components/Charts/LineChart/index.js
@@ -16,32 +16,29 @@ const generateOptions = (data) => {
       categories,
       crosshair: true,
     },
-    colors: ["#f3585B"],
     yAxis: {
       min: 0,
       title: {
         text: null,
       },
     },
-    tooltip: {
-      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-      pointFormat:
-        '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-        '<td style="padding:0"><b>{point.y} ca</b></td></tr>',
-      footerFormat: "</table>",
-      shared: true,
-      useHTML: true,
-    },
     plotOptions: {
-      column: {
-        pointPadding: 0.2,
-        borderWidth: 0,
+      series: {
+        stickyTracking: false,
       },
     },
     series: [
       {
         name: "Infected",
+        color: "#f3585B",
+        lineWidth: 5,
         data: data.map((item) => item.Confirmed),
+      },
+      {
+        name: "Deaths",
+        color: "#666666",
+        lineWidth: 5,
+        data: data.map((item) => item.Deaths),
       },
     ],
   };
